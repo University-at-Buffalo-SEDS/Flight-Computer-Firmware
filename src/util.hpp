8be691475d32bcaab4bc933246ee2f4a657ebf25
Owen Torres <55297187+optorres@users.noreply.github.com>
2019-10-28 21:24:14 -0400

    Remove unused data structures

--- src/util.hpp
+++ src/util.hpp
@@ -161,50 +161,6 @@ bool RingBuffer<T, Cap>::pop(T *data, size_t count) {
 	return true;
 }
 
-#if 0
-template <typename T, uint8_t S>
-class PtrStack {
-	T *arr[S];
-	uint8_t top;
-public:
-	PtrStack() : top(0) {}
-	bool push(T *v) {
-		if (top == S) return false;
-		arr[top++] = v;
-		return true;
-	}
-	T *pop() {
-		if (top == 0) return nullptr;
-		return arr[--top];
-	}
-};
-
-
-template <typename T, uint8_t S>
-class PtrQueue {
-	T *arr[S];
-	uint8_t head, tail, size;
-	uint8_t next(uint8_t i) { return i < (S - 1) ? i + 1 : 0; }
-public:
-	PtrQueue() : head(0), tail(0), size(0) {}
-	bool push(T *v) {
-		if (size >= S) return false;
-		arr[head] = v;
-		head = next(head);
-		++size;
-		return true;
-	}
-	T pop() {
-		if (size <= 0) return nullptr;
-		T *v = arr[tail];
-		tail = next(tail);
-		--size;
-		return v;
-	}
-	bool empty() { return head == tail; }
-};
-#endif
-
 #if DEBUG
 #define DEBUG_SECTION(x) x
 #else
